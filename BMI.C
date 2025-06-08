6dd4127f53fe6453d5df0caca052dd75c5e315b3
matiastorrez99111 <matitorrez99111@gmail.com>
2025-06-08 19:33:40 -0300

    Update BMI.C

--- BMI.C
+++ BMI.C
@@ -17,6 +17,12 @@ int main() {
 
     imc = peso / (altura * altura);
 
+printf("\nTabla de IMC");
+printf("\n menor a 18.5-->Bajo peso");
+printf("\n entre 18,5 y 24,9-->Normal");
+printf("\n entre 25,0 y 29,9-->Sobrepeso");
+printf("\n mayor a 30--> Obesidad");
+
     printf("\nSu IMC es: %.2f\n", imc);
     printf("Clasificación según el IMC:\n");
 
@@ -29,11 +35,5 @@ int main() {
     else if (imc < 35.0)
         printf("Obesidad \n");
    
-printf("\nTabla de IMC");
-printf("\n menor a 18.5-->Bajo peso");
-printf("\n entre 18,5 y 24,9-->Normal");
-printf("\n entre 25,0 y 29,9-->Sobrepeso");
-printf("\n mayor a 30--> Obesidad");
-
     return 0;
 }
